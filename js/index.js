1b8856d068658351614bb649b654e27bdd877c0b
luisfeCL <92877836+luisfeCL@users.noreply.github.com>
2022-03-30 15:29:10 +0200

    todo

---
+++ js/index.js
@@ -0,0 +1,22 @@
+import { swiper } from './swiper.js';
+
+
+const hamburguerButton = document.querySelector('.hamburguer-bt');
+const menu = document.querySelector('.menu');
+const subMenuButtonProducts = document.querySelector('.products');
+const subMenuButtonAbout = document.querySelector('.about');
+const subMenuProducts = document.querySelector('.submenuproducts');
+const subMenuAbout = document.querySelector('.submenuabout')
+
+function hamburgueMenu(){hamburguerButton.addEventListener('click', function() {
+    hamburguerButton.classList.toggle('on');
+    menu.classList.toggle('off')
+})
+}
+hamburgueMenu();
+subMenuButtonProducts.addEventListener('click', function() {
+    subMenuProducts.classList.toggle('off')
+})
+subMenuButtonAbout.addEventListener('click', function() {
+    subMenuAbout.classList.toggle('off')
+})
