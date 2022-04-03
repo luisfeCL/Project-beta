e39d7af0dd0ce1459f9e42ec775f73c36ac091fb
luisfeCL <92877836+luisfeCL@users.noreply.github.com>
2022-04-03 23:03:05 +0200

    maquetación y apertura carrito

--- js/single-product.js
+++ js/single-product.js
@@ -3,17 +3,36 @@ const menu = document.querySelector('.menu');
 const subMenuButtonProducts = document.querySelector('.products');
 const subMenuButtonAbout = document.querySelector('.about');
 const subMenuProducts = document.querySelector('.submenuproducts');
-const subMenuAbout = document.querySelector('.submenuabout')
+const subMenuAbout = document.querySelector('.submenuabout');
+const backpackButton = document.querySelector('.header__backpack');
+const backpack = document.querySelector('.backpack')
 
 function hamburgueMenu(){hamburguerButton.addEventListener('click', function() {
-    hamburguerButton.classList.toggle('on');
-    menu.classList.toggle('off')
+    if(backpack.classList.contains('off')){
+        hamburguerButton.classList.toggle('on');
+        menu.classList.toggle('off')
+    }else{
+        backpack.classList.toggle('off');
+        hamburguerButton.classList.toggle('on');
+        menu.classList.toggle('off')
+    }
 })
 }
 hamburgueMenu();
+
+backpackButton.addEventListener('click', function(){
+    if (menu.classList.contains('off')){
+        backpack.classList.toggle('off')
+    }else{
+        menu.classList.toggle('off');
+        backpack.classList.toggle('off')
+    }
+})
+
 subMenuButtonProducts.addEventListener('click', function() {
     subMenuProducts.classList.toggle('off')
 })
+
 subMenuButtonAbout.addEventListener('click', function() {
     subMenuAbout.classList.toggle('off')
 })
--- js/tienda.js
+++ js/tienda.js
@@ -5,22 +5,38 @@ const subMenuButtonProducts = document.querySelector('.products');
 const subMenuButtonAbout = document.querySelector('.about');
 const subMenuProducts = document.querySelector('.submenuproducts');
 const subMenuAbout = document.querySelector('.submenuabout');
+const backpackButton = document.querySelector('.header__backpack');
+const backpack = document.querySelector('.backpack')
 
-function hamburgueMenu(){
-    hamburguerButton.addEventListener('click', function() {
-    hamburguerButton.classList.toggle('on');
-    menu.classList.toggle('off')
+function hamburgueMenu(){hamburguerButton.addEventListener('click', function() {
+    if(backpack.classList.contains('off')){
+        hamburguerButton.classList.toggle('on');
+        menu.classList.toggle('off')
+    }else{
+        backpack.classList.toggle('off');
+        hamburguerButton.classList.toggle('on');
+        menu.classList.toggle('off')
+    }
 })
 }
 hamburgueMenu();
 
+backpackButton.addEventListener('click', function(){
+    if (menu.classList.contains('off')){
+        backpack.classList.toggle('off')
+    }else{
+        menu.classList.toggle('off');
+        backpack.classList.toggle('off')
+    }
+})
+
 subMenuButtonProducts.addEventListener('click', function() {
     subMenuProducts.classList.toggle('off')
 })
+
 subMenuButtonAbout.addEventListener('click', function() {
     subMenuAbout.classList.toggle('off')
 })
-
 
 const printProduct = function(elemento){
     return `<a href='./single_product.html?product=${elemento.name}' class="tienda__item grid__cell--20" >
